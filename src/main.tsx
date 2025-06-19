f71a833b6bf370972e936f7a93f4e942cecb05fa
Aaron Beall <contact@abeall.com>
2025-06-19 01:08:47 -0400

    fix: Update paths in index.html and main.tsx for relative linking

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
-import { BrowserRouter as Router } from 'react-router-dom'; // Import Router
+import { BrowserRouter } from 'react-router-dom'; // Import Router
 import theme from './theme'; // Import the custom theme
 import App from './App';
 import './index.css';
 
+const base = import.meta.env.BASE_URL;
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   // <React.StrictMode>
-    <Router>
+    <BrowserRouter basename={base}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <ChakraProvider theme={theme}>
         <App />
       </ChakraProvider>
-    </Router>
+    </BrowserRouter>
   // </React.StrictMode>
 );
